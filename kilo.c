81e8101306b0c132e8a51f9484eeacbfdc5418b0
Doug Dlutz <djd3681@rit.edu>
2018-05-22 01:08:00 -0700

    Reset terminal on exit.

--- kilo.c
+++ kilo.c
@@ -20,6 +20,8 @@ struct termios orig_termios;
 
 void die(const char* s)
 {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
     perror(s);
     exit(1);
 }
@@ -74,6 +76,7 @@ char editorReadKey()
 void editorRefreshScreen()
 {
     write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
 /*** input ***/
@@ -85,6 +88,8 @@ void editorProcessKeypress()
     switch (c)
     {
         case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
     }
